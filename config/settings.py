84808d039b4e575e18171c56e88cce435b3029d5
Keyvan Mosharraf <keyvan@keyvanm.com>
2023-03-02 20:00:05 -0800

    the cookiecutter setup.

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'todothings.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'todothings.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
