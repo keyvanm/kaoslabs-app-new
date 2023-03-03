597fd3bcbfed20293a0efdea8612e38b2f7a1080
Keyvan Mosharraf <keyvan@keyvanm.com>
2023-03-02 23:49:19 -0800

    Basic users app

--- config/settings/base.py
+++ config/settings/base.py
@@ -89,6 +89,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'users.User'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -96,4 +98,5 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kaoslabs_app.users',
 ]
